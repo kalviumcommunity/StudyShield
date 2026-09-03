49c9b5a45e971eee80101f8e048d215b90b5c87a
Akshat <akshat.sahu.s.125@kalvium.community>
2026-09-03 20:27:01 +0530

    fix:removed prisma.config.js code from postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,15 +1,5 @@
-import "dotenv/config";
-import { defineConfig, env } from "prisma/config";
-
-export default defineConfig({
-  schema: "prisma/schema.prisma",
-
-  migrations: {
-    path: "prisma/migrations",
-    seed: "tsx prisma/seed.js",
+export default {
+  plugins: {
+    "@tailwindcss/postcss": {},
   },
-
-  datasource: {
-    url: env("DIRECT_URL"),
-  },
-});
+};
